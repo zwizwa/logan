8aed4aed1c7c8fc6660ef19bb7252c2bdeed8d9a
Tom Schouten <tom@zwizwa.be>
2015-02-03 14:50:16 -0500

    diff test

--- la.rs
+++ la.rs
@@ -239,9 +239,19 @@ fn main_uart() {
 }
 
 fn main_diff() {
+    use std::old_io;
+    let mut out = old_io::stdout();
+    
     let diff = diff::init();
     for b in la::proc_map(diff, io::stdin8()) {
-        println!("{0:x}", b);
+        match out.write_all(format!(" {0:x}", b).as_bytes()) {
+            Err(err) => panic!("{}",err),
+            Ok(_) => (),
+        }
+        match out.flush() {
+            Err(err) => panic!("{}",err),
+            Ok(_) => (),
+        }
     }
 }
 

0ba737ea84bfcf0d9028a806a19df9f075e6d98d
Tom Schouten <tom@zwizwa.be>
2015-02-26 05:21:14 +0000

    rle.c

--- examples/rle.c
+++ examples/rle.c
@@ -1,10 +1,13 @@
+// simple test that reads and walks the data.
+// C isn't fast enough
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned char buf[4*1024*1024];
+unsigned int buf[1*1024*1024];
 
 void error_exit(const char *msg) {
     fprintf(stderr, "assert failed: %s\n", msg);
@@ -13,11 +16,12 @@ void error_exit(const char *msg) {
 #define A(it) {if (!(it)) error_exit(#it);}
 
 void main(int argc, char **argv) {
-    int fd,nb;
+    int fd,nb,i,acc=0;
     A( argc == 2 );
     A( (fd = open(argv[1], O_RDONLY)) >= 0 );
     for(;;) {
-        A( (nb = read(fd, buf, sizeof(buf)))> 0 );
-        fprintf(stderr, "%d ", nb);
+        A( (nb = read(fd, buf, sizeof(buf))) > 0 );
+        for(i=0;i<nb/4;i++) { acc += buf[i]; }
+        fprintf(stderr, "%d ", acc);
     }
 }

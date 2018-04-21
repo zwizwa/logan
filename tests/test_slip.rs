4f5b21c57a929656d33a08ee22f526909f921ae2
Tom Schouten <tom@zwizwa.be>
2018-04-21 18:47:51 -0400

    test fixes

---
+++ tests/test_slip.rs
@@ -0,0 +1,36 @@
+extern crate la;
+use la::tick::apply;
+use la::tick::slip;
+
+
+fn test1() {
+    let mut slip = slip::init(
+        slip::Config{
+            end: 0x0D,
+            esc: 0x0C,
+            esc_end: 0x0B,
+            esc_esc: 0x0A,
+        }
+    );
+    let mut test_data = [0x0D,1,2,3,0x0D,4,5,6,0xD].iter();
+    let data_out: Vec<_> = apply(&mut slip, &mut test_data).collect();
+
+    let data_expected: Vec<Vec<u8>> = [
+        [].to_vec(),
+        [1,2,3].to_vec(),
+        [4,5,6].to_vec()
+    ].to_vec();
+
+    assert_eq!(data_out, data_expected);
+
+    println!("slip OK");
+}
+
+fn main() {
+    test1();
+}
+
+#[test]
+fn run_tests() {
+    main()
+}
